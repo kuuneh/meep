0e1085e8076c94070813d0704d03db73c423579c
kuuneh <dreadcracked992@gmail.com>
2025-08-04 19:36:32 -0400

    trying again

--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-
-    // Handle an API route
-    if (url.pathname === "/api/hello") {
-      return new Response(JSON.stringify({ message: "Hello from Worker!" }), {
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-
-    // Otherwise, serve static file from dist/
+    // Automatically serve files from your dist folder
     return env.ASSETS.fetch(request);
   },
 };
